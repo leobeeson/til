d123d46942d1f20173f3f0ed40cf384485107ad1
Francis Beeson <leobeeson@gmail.com>
2023-01-22 23:23:49 +0000

    Add function to convert from decimal to hexadecimal.

--- sde/operations/networks/binary_calculator.py
+++ sde/operations/networks/binary_calculator.py
@@ -13,7 +13,35 @@ def convert_binary_to_decimal(binary_number: int) -> None:
     print(decimal_number)
 
 
+hexadecimal_table = {
+    0: "0",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
+    10: "A",
+    11: "B",
+    12: "C",
+    13: "D",
+    14: "E",
+    15: "F",
+}
+
+def convert_decimal_to_hexadecimal(decimal_number) -> str:
+    if (decimal_number <= 0):
+        return ""
+    remainder = decimal_number % 16
+    return convert_decimal_to_hexadecimal(decimal_number//16) + hexadecimal_table[remainder]
+
+
 if __name__ == "__main__":
     convert_decimal_to_binary(192)
     print("\n")
     convert_binary_to_decimal(11000000)
+    print("\n")
+    print(convert_decimal_to_hexadecimal(2290))
